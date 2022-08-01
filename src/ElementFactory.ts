003c4b128346a7e9dad074647c0498f3b27a96a8
Vinlic <vinlic@vinlic.com>
2022-08-01 23:28:47 +0800

    增加Group

--- src/ElementFactory.ts
+++ src/ElementFactory.ts
@@ -1,5 +1,5 @@
 import IElementOptions from "./elements/interface/IElementOptions";
-import { Element } from "./elements";
+import { Element, Group } from "./elements";
 import ElementTypes from "./enums/ElementTypes";
 import util from "./util";
 
@@ -8,7 +8,8 @@ export default class ElementFactory {
     static createElement(data: IElementOptions) {
         if (!util.isObject(data)) throw new TypeError('data must be an Object');
         switch(data.type) {
-            
+            case ElementTypes.Group:
+                return new Group(data);
         }
         return new Element(data);
     }
