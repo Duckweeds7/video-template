9ba79e038b54b38fbc230ff93e9801834e16949c
Vinlic <vinlic@vinlic.com>
2022-08-03 10:17:41 +0800

    修正媒体属性类型

--- src/elements/Media.ts
+++ src/elements/Media.ts
@@ -9,14 +9,14 @@ export default class Media extends Element {
     poster?: string;  //媒体封面
     src?: string;  //媒体来源
     path?: string;  //媒体路径
-    volume?: number | string;  //媒体音量
+    volume?: number;  //媒体音量
     format?: string;  //媒体格式
-    duration?: number | string;  //媒体时长（毫秒）
-    seekStart?: number | string;  //媒体截取开始时间点（毫秒）
-    seekEnd?: number | string;  //媒体截取结束时间点（毫秒）
-    loop?: boolean | string;  //媒体是否循环播放
-    playbackRate?: number | string;  //媒体播放倍速（0-2，默认1）
-    muted?: boolean | string;  //媒体是否静音
+    duration?: number;  //媒体时长（毫秒）
+    seekStart?: number;  //媒体截取开始时间点（毫秒）
+    seekEnd?: number;  //媒体截取结束时间点（毫秒）
+    loop?: boolean;  //媒体是否循环播放
+    playbackRate?: number;  //媒体播放倍速（0-2，默认1）
+    muted?: boolean;  //媒体是否静音
 
     constructor(options: IMediaOptions) {
         super(options);
